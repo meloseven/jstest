5dd4429fdff91999fcb9374468776238cecf8a0c
melo <1321340518@qq.com>
2017-06-07 23:22:11 +0800

    测试二进制与八进制

--- es6/src/t4.js
+++ es6/src/t4.js
@@ -1,3 +1,10 @@
 /**
  * Created by melo on 2017/5/23.
  */
+var log = console.log;
+let r1 = (0b11110111 === 247);
+let r2 = (0o25 === 21);
+log(r1);
+log(r2);
+log(Number(0b11110111));
+log(Number(0o25));
